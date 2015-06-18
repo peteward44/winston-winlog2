5393c59647ef6efa9e37a6e92e3f834af94d59e2
pete <peteward44@gmail.com>
2015-06-18 12:10:23 +0100

    Added tests, remove newlines as they don't work with node-windows

---
+++ test/test.js
@@ -0,0 +1,50 @@
+/*global describe:false, it:false */
+'use strict';
+
+var winston = require('winston');
+var winlog = require('../');
+
+
+describe('winston-winlog2', function() {
+	
+	before( function() {
+		winston.add( winlog, {} );
+	} );
+	
+	it('info', function(done) {
+		winston.info( 'info message', function( err ) {
+			done( err );
+		} );
+	});
+	
+	it('warn', function(done) {
+		winston.warn( 'warning message', function( err ) {
+			done( err );
+		} );
+	});
+	
+	it('error', function(done) {
+		winston.error( 'error message', function( err ) {
+			done( err );
+		} );
+	});
+		
+	it('info newline', function(done) {
+		winston.info( 'info message\r\nnewline', function( err ) {
+			done( err );
+		} );
+	});
+	
+	it('warn newline', function(done) {
+		winston.warn( 'warning message\r\nnewline', function( err ) {
+			done( err );
+		} );
+	});
+	
+	it('error newline', function(done) {
+		winston.error( 'error message\r\nnewline', function( err ) {
+			done( err );
+		} );
+	});
+});
+
